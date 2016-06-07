d148b12bfdd1bf0728f7da2354a2f2ca8a6e38a5
h.matsuo <matsuoshi@gmail.com>
2016-06-07 19:49:46 +0900

    init

---
+++ postical.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Plugin Name: postical
+ * Version: 0.9
+ * Description: export the posts schedules as iCal format.
+ * Author: h.matsuo
+ * Author URI: http://github.com/matsuoshi
+ * Plugin URI: http://github.com/matsuoshi/postical
+ * Text Domain: postical
+ * Domain Path: /languages
+ * @package Postical
+ */
